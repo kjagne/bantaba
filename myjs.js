8f2c12c63180b7ed54007333d1bb1c9dcb73f8d4
kjagne <kjagne@github.com>
2017-11-24 15:07:59 -0800

    f

--- myjs.js
+++ myjs.js
@@ -1,4 +1,42 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+var user;
 var selectedFile;
+
+
+$( document ).ready(function() {
+	$("#welcome").hide();
+	$("#uploadButton").hide();
+	$(".upload-group").hide();
+});
+
+function signIn() {
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.user.uid;
+	  // The signed-in user info.
+	  user = result.user;
+	  showWelcomeContainer();
+	  sessionStorage.token = token;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+
+};
+
+function showWelcomeContainer() {
+	$("#login").hide();
+	$("#welcome").show();
+	$(".upload-group").show();
+	$("#welcomeText").html("Hello, " + user.displayName);
+};
 
 $(".dropdown").on("hide.bs.dropdown", function(event){
     var text = $(event.relatedTarget).text(); // Get the text of the element
@@ -6,7 +44,6 @@ $(".dropdown").on("hide.bs.dropdown", function(event){
     firebase.database().ref('Users/' + user.uid).set({
     	name: user.displayName,
     	email: user.email,
-    	password: user.password,
     	favDog: text
   	});
 
@@ -20,7 +57,7 @@ $("#file").on("change", function(event) {
 function uploadFile() {
 	// Create a root reference
 	var filename = selectedFile.name;
-	var storageRef = firebase.storage().ref('/PostImages/' + filename);
+	var storageRef = firebase.storage().ref('/dogImages/' + filename);
 	var uploadTask = storageRef.put(selectedFile);
 
 	// Register three observers:
@@ -48,3 +85,7 @@ function uploadFile() {
 	});
 
 }
+
+
+
+
